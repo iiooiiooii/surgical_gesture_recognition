f8c27aa732026038fd704eb74c52b9f02b46fa0f
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-03-29 09:04:43 +0430

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,3 +2,6 @@ import os
 import time
 from sys import stderr
 
+
+splits_LOSO = ['data_1.csv', 'data_2.csv', 'data_3.csv', 'data_4.csv', 'data_5.csv']
+splits_LOUO = ['data_B.csv', 'data_C.csv', 'data_D.csv', 'data_E.csv', 'data_F.csv', 'data_G.csv', 'data_H.csv', 'data_I.csv']
