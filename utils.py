7a022fdf995cf0d71a0457593e56cb0e16119cd5
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-03-29 09:05:24 +0430

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,6 @@ splits_LOSO = ['data_1.csv', 'data_2.csv', 'data_3.csv', 'data_4.csv', 'data_5.c
 splits_LOUO = ['data_B.csv', 'data_C.csv', 'data_D.csv', 'data_E.csv', 'data_F.csv', 'data_G.csv', 'data_H.csv', 'data_I.csv']
 
 splits_LOUO_NP = ['data_B.csv', 'data_C.csv', 'data_D.csv', 'data_E.csv', 'data_F.csv', 'data_H.csv', 'data_I.csv']
+
+gestures_SU = ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G8', 'G9', 'G10', 'G11']
+
