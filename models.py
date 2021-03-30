695934e8d4f1a2441e257692371ef2869a6bbcd7
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-03-30 20:05:29 +0430

    Update models.py

--- models.py
+++ models.py
@@ -4,3 +4,8 @@ import torchvision
 from torch import nn
 from torch.nn.init import normal, constant
 
+from resnet3D import resnet18
+from transforms import GroupMultiScaleCrop, GroupRandomHorizontalFlip
+from train_opts import num_cls_Kinetics
+
+
