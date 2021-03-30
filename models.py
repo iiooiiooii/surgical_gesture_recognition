e3405c2c3d91876110e3d7993271c83d690eb6d1
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-03-30 20:07:25 +0430

    Update models.py

--- models.py
+++ models.py
@@ -9,3 +9,12 @@ from transforms import GroupMultiScaleCrop, GroupRandomHorizontalFlip
 from train_opts import num_cls_Kinetics
 
 
+class GestureClassifier(nn.Module):
+    def __init__(self, base_model, num_class, modality, dropout=0.8, snippet_length=16, input_size=112,
+                 pretrained_model=None, bootstrap_from_2D=False, use_resnet_shortcut_type_B=False):
+        super(GestureClassifier, self).__init__()
+        self.arch = base_model
+        self.modality = modality
+        
+        self._init_base_model(base_model, num_class, dropout, snippet_length, input_size,
+                              pretrained_model, bootstrap_from_2D, use_resnet_shortcut_type_B)
