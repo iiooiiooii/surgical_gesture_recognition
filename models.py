c380dd8809e447d239b28d1a02b894803f098eab
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-03-30 20:08:40 +0430

    Create models.py

--- models.py
+++ models.py
@@ -18,3 +18,15 @@ class GestureClassifier(nn.Module):
         
         self._init_base_model(base_model, num_class, dropout, snippet_length, input_size,
                               pretrained_model, bootstrap_from_2D, use_resnet_shortcut_type_B)
+
+    def _init_base_model(self, base_model, num_class, dropout=0.8, snippet_length=16, input_size=112,
+                         pretrained_model=None, bootstrap_from_2D=False, use_resnet_shortcut_type_B=False):
+        if base_model == '3D-ResNet-18':
+            self.snippet_length = snippet_length
+            self.input_size = input_size
+            self.input_mean = [0.485, 0.456, 0.406]
+            self.input_std = [0.229, 0.224, 0.225]
+            if self.modality == 'Flow':
+                self.input_mean = [0.5]
+                self.input_std = [np.mean(self.input_std)]
+
