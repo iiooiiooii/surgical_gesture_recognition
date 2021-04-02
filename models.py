e7a6bac2effb965b77f9fbf48ba293fde83bea5d
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-04-02 07:40:01 +0430

    Update models.py

--- models.py
+++ models.py
@@ -118,3 +118,4 @@ class GestureClassifier(nn.Module):
             out = base_out
         else:
             out = base_out.view(base_out.size(0), base_out.size(1), -1)
+            out = self.up_conv(out)
