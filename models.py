57cf88e28d52e73dd9f658a9a107fe456669421c
Mohammad Mahdi Khorsand Aalam <75268577+iiooiiooii@users.noreply.github.com>
2021-04-04 01:17:19 +0430

    Update models.py

--- models.py
+++ models.py
@@ -166,3 +166,4 @@ class GestureClassifier(nn.Module):
 
         if in_channels == -1:
             in_channels = 2 * self.snippet_length
+        # modify parameters, assume the first blob contains the convolution kernels
